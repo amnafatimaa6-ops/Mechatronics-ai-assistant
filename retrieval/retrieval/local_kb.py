c9c9ba39e102dce3127818766ca2efd224e81460
Amna Fatima <amnafatimaa6@gmail.com>
2026-04-17 23:09:00 -0700

    Create local_kb.py

---
+++ retrieval/retrieval/local_kb.py
@@ -0,0 +1,15 @@
+def get_local_context(query):
+    kb = """
+DC motor overheating causes:
+- High current draw
+- Mechanical friction
+- Poor cooling
+- Overload conditions
+
+Torque issues:
+- Voltage drop
+- Gear mismatch
+- Load increase
+"""
+
+    return kb
