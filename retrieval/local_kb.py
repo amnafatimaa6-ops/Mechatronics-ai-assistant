98f1bc4f59115afe6eaa042961acc453d82f687e
Amna Fatima <amnafatimaa6@gmail.com>
2026-04-17 23:18:20 -0700

    Create local_kb.py

---
+++ retrieval/local_kb.py
@@ -0,0 +1,19 @@
+def get_local_context(query):
+    return """
+DC motor overheating:
+- caused by high current draw
+- mechanical overload
+- poor cooling system
+- worn bearings increase friction
+
+Torque issues:
+- low voltage supply
+- gear mismatch
+- excessive load
+- controller tuning issues
+
+Sensor errors:
+- noise interference
+- poor calibration
+- wiring issues
+"""
